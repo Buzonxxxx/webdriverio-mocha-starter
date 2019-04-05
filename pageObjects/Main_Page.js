f5ecae928fcfb9366cfddfa03bdcfd2629d512a0
louis.liao <buzonliao@gmail.com>
2019-04-05 22:34:17 +0800

    refactor webdriverUniversityTest to pageObj

---
+++ pageObjects/Main_Page.js
@@ -0,0 +1,49 @@
+class Main_Page {
+  get contactUs() { return $("#contact-us");}
+  get loginPortal() { return $("#login-portal");}
+
+  getTitle() {
+    return browser.getTitle();
+  }
+  getTabIDs() {
+    return browser.getTabIds();
+  }
+  switchTab(tabID) {
+    return browser.switchTab(tabID);
+  }
+  getUrl() {
+    return browser.getUrl();
+  }
+  close() {
+    return browser.close();
+  }
+  verifyMainPageTitle() {
+    const title = this.getTitle();
+    expect(title).to.equal('WebDriverUniversity.com') 
+    console.log(`Title is: ${title}`);
+  }  
+  verifyContactUSPageTitle() {
+    this.contactUs.click();
+    const tabIDs = this.getTabIDs();
+    console.log(tabIDs);
+    this.switchTab(tabIDs[1]);
+    const title = this.getTitle();
+    expect(title).to.equal('WebDriver | Contact Us') 
+  }
+  verifyLoginPortalPageTitle() {
+    this.loginPortal.click();
+    const tabIDs = this.getTabIDs();
+    this.switchTab(tabIDs[1]);
+    const title = this.getTitle();
+    expect(title).to.equal('WebDriver | Login Portal') 
+  }
+  verifyContactUSPageUrl() {
+    const url = this.getUrl();
+    expect(url).include('Contact-Us', 'URL Mismatch')
+  }
+  verifyLoginPortalPageUrl() {
+    const url = this.getUrl();
+    expect(url).include('Login-Portal', 'URL Mismatch');
+  }
+}
+module.exports = new Main_Page();
--- tests/webdriverUniversityTest.js
+++ tests/webdriverUniversityTest.js
@@ -1,41 +1,18 @@
+const Main_Page = require('../pageObjects/Main_Page');
+beforeEach(() => {
+  browser.url('/')
+})
+
 describe('Verify whether webdriveruniversity links on homepage work correctly', () => {
   it('check that the contact us button opens the contact us page',  () => {
-    // browser.setViewportSize({
-    //   width: 1200,
-    //   height: 800
-    // })
-    browser.url('/')
-    let title = browser.getTitle()
-    assert.equal(title, 'WebDriverUniversity.com') //assert
-    expect(title).to.equal('WebDriverUniversity.com') //expect
-    title.should.equal('WebDriverUniversity.com') //chai
-    console.log(`Title is: ${title}`)
-    // browser.debug()
-
-    browser.click('#contact-us')
-    const tabIds = browser.getTabIds();
-    console.log(tabIds);
-    browser.switchTab(tabIds[1]);
-    const title2 = browser.getTitle()
-    assert.equal(title2, 'WebDriver | Contact Us') 
-
-    const url = browser.getUrl();
-    expect(url).include('Contact-Us', 'URL Mismatch')
-    browser.close();
+    Main_Page.verifyMainPageTitle();
+    Main_Page.verifyContactUSPageTitle();
+    Main_Page.verifyContactUSPageUrl();
+    Main_Page.close();
   })
   it('check that the login button opens the login portal page',  () => {
-    browser.url('/')
-    const title = browser.getTitle()
-    title.should.equal('WebDriverUniversity.com')
-    console.log(`Title is: ${title}`)
-
-    browser.click('#login-portal')
-    const tabIds = browser.getTabIds();
-    browser.switchTab(tabIds[1]);
-    const title2 = browser.getTitle()
-    assert.equal(title2, 'WebDriver | Login Portal') 
-
-    const url = browser.getUrl();
-    expect(url).include('Login-Portal', 'URL Mismatch')
+    Main_Page.verifyMainPageTitle();
+    Main_Page.verifyLoginPortalPageTitle();
+    Main_Page.verifyLoginPortalPageUrl();
   })
 })
