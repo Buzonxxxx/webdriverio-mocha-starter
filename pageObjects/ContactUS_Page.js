da77d31aa98d07a0d555853a8b3800bff6620e68
louis.liao <buzonliao@gmail.com>
2019-04-05 21:50:28 +0800

    move functions to contactUS_Page

--- pageObjects/ContactUS_Page.js
+++ pageObjects/ContactUS_Page.js
@@ -6,6 +6,36 @@ class ContactUs_Page {
   get submitButton() { return $('[type="submit"]');}
   get successfulSubmissionHeader() { return $("#contact_reply h1");}
   get unsuccessfulSubmissionHeader() { return $("body");}
-}
 
+  setFirstName(first_name) {
+    return this.firstName.setValue(first_name);
+  }  
+  setLastName(last_name) { 
+    return this.lastName.setValue(last_name);
+  }
+  setEmail(email) {
+    return this.email.setValue(email);
+  } 
+  setMessage(message) {
+    return this.message.setValue(message);
+  }
+  clickSubmitButton() {
+    return this.submitButton.click();
+  } 
+  submitAllInformation(firstName, lastName, email, message) {
+    if(firstName) this.setFirstName(firstName);
+    if(lastName) this.setLastName(lastName);
+    if(email) this.setEmail(email);
+    if(message) this.setMessage(message);
+    this.clickSubmitButton();
+  }
+  confirmSuccessfulSubmission() {
+    const validateSubmissionHeader = browser.waitUntil( () => this.successfulSubmissionHeader.getText() == "Thank You for your Message!", 3000);
+    expect(validateSubmissionHeader, "Successful submission Message does not exist!").to.be.true;
+  };
+  
+  confirmUnsuccessfulSubmission() {
+    expect(this.unsuccessfulSubmissionHeader.getText()).to.include("Error: all fields are required");
+  };
+}
 module.exports = new ContactUs_Page();
--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -6,53 +6,52 @@ beforeEach(() => {
 });
 
 describe("Test Contact Us form WebdriverUniversity", () => {
-  const res = request("GET", "https://jsonplaceholder.typicode.com/posts/1/comments");
+  const res = request(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts/1/comments"
+  );
   let contactDetails = JSON.parse(res.getBody().toString("utf8"));
   contactDetails.length = 3; // get first 3 contact details
 
-  const setFirstName = first_name => ContactUS_Page.firstName.setValue(first_name);
-  const setLastName = last_name => ContactUS_Page.lastName.setValue(last_name);
-  const setEmail = email => ContactUS_Page.email.setValue(email);
-  const setMessage = message => ContactUS_Page.message.setValue(message);
-  const clickSubmitButton = () => ContactUS_Page.submitButton.click();
-  const confirmSuccessfulSubmission = () => {
-    const validateSubmissionHeader = browser.waitUntil( () => ContactUS_Page.successfulSubmissionHeader.getText() == "Thank You for your Message!", 3000);
-    expect(validateSubmissionHeader, "Successful submission Message does not exist!").to.be.true;
-  };
-  const confirmUnsuccessfulSubmission = () => {
-    expect(ContactUS_Page.unsuccessfulSubmissionHeader.getText()).to.include("Error: all fields are required");
-  };
-
   contactDetails.forEach(contactDetail => {
     it("Should be able to submit a successful submission via contact us form", () => {
-      setFirstName("Louis");
-      setLastName("Liao");
-      setEmail(contactDetail.email);
-      setMessage(contactDetail.body);
-      clickSubmitButton();
-      confirmSuccessfulSubmission();
+      ContactUS_Page.submitAllInformation(
+        "Louis",
+        "Liao",
+        contactDetail.email,
+        contactDetail.body
+      );
+      ContactUS_Page.confirmSuccessfulSubmission();
     });
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-      setFirstName("Mike");
-      setLastName("Woods");
-      setEmail("mike_woods@gmail.com");
-      clickSubmitButton();
-      confirmUnsuccessfulSubmission();
+    ContactUS_Page.submitAllInformation(
+      "Mike",
+      "Woods",
+      "mike_woods@gmail.com",
+      null
+    );
+    ContactUS_Page.confirmUnsuccessfulSubmission();
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-      setFirstName("Sara");
-      setEmail("sara_woods@gmail.com");
-      clickSubmitButton();
-      confirmUnsuccessfulSubmission();
+    ContactUS_Page.submitAllInformation(
+      "Sara",
+      null,
+      "sara_woods@gmail.com",
+      null
+    );
+    ContactUS_Page.confirmUnsuccessfulSubmission();
   });
 
   it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-      setFirstName("Jim");
-      setLastName("James");
-      clickSubmitButton();
-      confirmUnsuccessfulSubmission();
+    ContactUS_Page.submitAllInformation(
+      "Jim,",
+      "James",
+      null,
+      null
+    );
+    ContactUS_Page.confirmUnsuccessfulSubmission();
   });
 });
