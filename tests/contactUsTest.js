47a2957be11f7a6a46efbe0e6f10c673e05cdb1a
louis.liao <buzonliao@gmail.com>
2019-04-05 08:37:35 +0800

    add addCommand example

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,5 +1,14 @@
 const request = require('sync-request');
 
+browser.addCommand("submitDataViaContactUSForm", (first_name, last_name, email, message) => {
+  if(first_name) browser.setValue("[name='first_name']", first_name);
+  if(last_name) browser.setValue("[name='last_name']", last_name);
+  if(email) browser.setValue("[name='email']", email);
+  if(message) browser.setValue("[name='message']", message);
+  browser.click('[type="submit"]');
+})
+
+
 beforeEach(() => {
 	browser.url('/Contact-Us/contactus.html');
 })
@@ -14,44 +23,32 @@ describe('Test Contact Us form WebdriverUni', () => {
     })
 contactDetails.forEach(contactDetail => {
   it('Should be able to submit a successful submission via contact us form', () => {
-    browser.setValue("[name='first_name']",'Louis');
-    browser.setValue("[name='last_name']",'Liao');
-    browser.setValue("[name='email']", contactDetail.email);
-    browser.setValue("[name='message']", contactDetail.body);
-    browser.click('[type="submit"]');
+    browser.submitDataViaContactUSForm('Louis', 'Liao', contactDetail.email, contactDetail.body);
 
     const successfulContactConfirmation = browser.isExisting("#contact_reply h1");
     assert.equal(successfulContactConfirmation, true, "Successful submission Message does not exist!");
+
     const successfulSubmission = browser.getText("#contact_reply h1");
     assert.equal(successfulSubmission, "Thank You for your Message!");
-
     });
-
 })    
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
-    browser.setValue("[name='first_name']",'Mike');
-    browser.setValue("[name='last_name']",'Woods');
-    browser.setValue("[name='email']",'mike_woods@gmail.com');
-    browser.click('[type="submit"]');
+    browser.submitDataViaContactUSForm('Mike', 'Woods', 'mike_woods@gmail.com', null);
 
     const successfulContactConfirmation = browser.isExisting("#contact_reply h1");
     assert.equal(successfulContactConfirmation, false);
     });
   
   it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
-    browser.setValue("[name='first_name']",'Sara');
-    browser.setValue("[name='email']",'sara_woods@gmail.com');
-    browser.click('[type="submit"]');
+    browser.submitDataViaContactUSForm('Sara', null, 'sara_woods@gmail.com', null);
 
     const successfulContactConfirmation = browser.isExisting("#contact_reply h1");
     assert.equal(successfulContactConfirmation, false);
     });
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
-    browser.setValue("[name='first_name']",'Jim');
-    browser.setValue("[name='last_name']",'James');
-    browser.click('[type="submit"]');
+    browser.submitDataViaContactUSForm('Jim', 'James', null, null);
 
     let errorText = browser.getText("body");
     expect(errorText).to.include('Error: all fields are required')
